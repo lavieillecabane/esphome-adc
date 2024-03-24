9a79cd5482b5faba3b406f573ad835e3941bb11a
Ludovic BOUÉ <lboue@users.noreply.github.com>
2024-03-24 17:15:29 +0100

    Update sensor.py

--- esphome/components/max17048/sensor.py
+++ esphome/components/max17048/sensor.py
@@ -1,21 +1,34 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import i2c, sensor, voltage_sampler
+from esphome.components import sensor, voltage_sampler
 from esphome.const import (
-    CONF_ID,
-    CONF_VOLTAGE,
+    CONF_GAIN,
     DEVICE_CLASS_VOLTAGE,
     STATE_CLASS_MEASUREMENT,
     UNIT_VOLT,
+    CONF_ID,
 )
+from . import max17048_ns, MAX17048Component
 
-DEPENDENCIES = ["i2c"]
-max17048_ns = cg.esphome_ns.namespace("max17048")
+DEPENDENCIES = ["max17048"]
 
-#define MAX17048_I2CADDR_DEFAULT 0x36 ///< MAX17048 default i2c address
-MAX17048Component = max17048_ns.class_(
-    "MAX17048Component", cg.PollingComponent, i2c.I2CDevice
-)
+MAX17048Gain = max17048_ns.enum("MAX17048Gain")
+GAIN = {
+    "6.144": MAX17048Gain.MAX17048_GAIN_6P144,
+    "4.096": MAX17048Gain.MAX17048_GAIN_4P096,
+    "2.048": MAX17048Gain.MAX17048_GAIN_2P048,
+    "1.024": MAX17048Gain.MAX17048_GAIN_1P024,
+    "0.512": MAX17048Gain.MAX17048_GAIN_0P512,
+    "0.256": MAX17048Gain.MAX17048_GAIN_0P256,
+}
+
+def validate_gain(value):
+    if isinstance(value, float):
+        value = f"{value:0.03f}"
+    elif not isinstance(value, str):
+        raise cv.Invalid(f'invalid gain "{value}"')
+
+    return cv.enum(GAIN)(value)
 
 MAX17048Sensor = max17048_ns.class_(
     "MAX17048Sensor", sensor.Sensor, cg.PollingComponent, voltage_sampler.VoltageSampler
@@ -33,40 +46,17 @@ CONFIG_SCHEMA = (
     .extend(
         {
             cv.GenerateID(CONF_MAX17048_ID): cv.use_id(MAX17048Component),
-            #cv.Required(CONF_MULTIPLEXER): cv.enum(MUX, upper=True, space="_"),
-            #cv.Required(CONF_GAIN): validate_gain,
-            #cv.Optional(CONF_RESOLUTION, default="16_BITS"): cv.enum(
-            #    RESOLUTION, upper=True, space="_"
-            #),
+            cv.Required(CONF_GAIN): validate_gain,
         }
     )
     .extend(cv.polling_component_schema("60s"))
-    .extend(i2c.i2c_device_schema(0x36))
 )
-'''
-CONFIG_SCHEMA = (
-    cv.Schema(
-        {
-            cv.GenerateID(): cv.declare_id(MAX17048Component),
-            cv.Optional(CONF_VOLTAGE): sensor.sensor_schema(
-                unit_of_measurement=UNIT_VOLT,
-                accuracy_decimals=2,
-                device_class=DEVICE_CLASS_VOLTAGE,
-                state_class=STATE_CLASS_MEASUREMENT,
-            ),
-        }
-    )
-    .extend(cv.polling_component_schema("60s"))
-    .extend(i2c.i2c_device_schema(0x36))
-)
-'''
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
+    paren = await cg.get_variable(config[CONF_MAX17048_ID])
+    var = cg.new_Pvariable(config[CONF_ID], paren)
+    await sensor.register_sensor(var, config)
     await cg.register_component(var, config)
-    await i2c.register_i2c_device(var, config)
 
-    if CONF_VOLTAGE in config:
-        conf = config[CONF_VOLTAGE]
-        sens = await sensor.new_sensor(conf)
-        cg.add(var.set_voltage_sensor(sens))
+    cg.add(var.set_gain(config[CONF_GAIN]))
+    cg.add(paren.register_sensor(var))
