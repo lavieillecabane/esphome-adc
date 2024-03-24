6f35961c0cbea6fbc4f5796fe62726974c1d8571
Ludovic BOUÉ <lboue@users.noreply.github.com>
2024-03-24 11:26:02 +0100

    Update sensor.py

--- esphome/components/max17048/sensor.py
+++ esphome/components/max17048/sensor.py
@@ -1,12 +1,12 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import i2c, sensor
+from esphome.components import i2c, sensor, voltage_sampler
 from esphome.const import (
     CONF_ID,
     CONF_VOLTAGE,
     DEVICE_CLASS_VOLTAGE,
-    UNIT_VOLT,
     STATE_CLASS_MEASUREMENT,
+    UNIT_VOLT,
 )
 
 DEPENDENCIES = ["i2c"]
@@ -17,6 +17,33 @@ MAX17048Component = max17048_ns.class_(
     "MAX17048Component", cg.PollingComponent, i2c.I2CDevice
 )
 
+MAX17048Sensor = max17048_ns.class_(
+    "MAX17048Sensor", sensor.Sensor, cg.PollingComponent, voltage_sampler.VoltageSampler
+)
+
+CONF_MAX17048_ID = "max17048_id"
+CONFIG_SCHEMA = (
+    sensor.sensor_schema(
+        MAX17048Sensor,
+        unit_of_measurement=UNIT_VOLT,
+        accuracy_decimals=3,
+        device_class=DEVICE_CLASS_VOLTAGE,
+        state_class=STATE_CLASS_MEASUREMENT,
+    )
+    .extend(
+        {
+            cv.GenerateID(CONF_MAX17048_ID): cv.use_id(MAX17048Component),
+            #cv.Required(CONF_MULTIPLEXER): cv.enum(MUX, upper=True, space="_"),
+            #cv.Required(CONF_GAIN): validate_gain,
+            #cv.Optional(CONF_RESOLUTION, default="16_BITS"): cv.enum(
+            #    RESOLUTION, upper=True, space="_"
+            #),
+        }
+    )
+    .extend(cv.polling_component_schema("60s"))
+    .extend(i2c.i2c_device_schema(0x36))
+)
+'''
 CONFIG_SCHEMA = (
     cv.Schema(
         {
@@ -32,8 +59,14 @@ CONFIG_SCHEMA = (
     .extend(cv.polling_component_schema("60s"))
     .extend(i2c.i2c_device_schema(0x36))
 )
+'''
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await i2c.register_i2c_device(var, config)
+
+    if CONF_VOLTAGE in config:
+        conf = config[CONF_VOLTAGE]
+        sens = await sensor.new_sensor(conf)
+        cg.add(var.set_voltage_sensor(sens))
