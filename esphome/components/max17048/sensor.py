d9b07314d587fc1139e307201c3e370433748592
Ludovic BOUÉ <lboue@users.noreply.github.com>
2024-03-24 10:29:30 +0100

    Create sensor.py
    MAX17048Component

---
+++ esphome/components/max17048/sensor.py
@@ -0,0 +1,39 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import i2c, sensor
+from esphome.const import (
+    CONF_ID,
+    CONF_VOLTAGE,
+    DEVICE_CLASS_VOLTAGE,
+    UNIT_VOLT,
+    STATE_CLASS_MEASUREMENT,
+)
+
+DEPENDENCIES = ["i2c"]
+max17048_ns = cg.esphome_ns.namespace("max17048")
+
+#define MAX17048_I2CADDR_DEFAULT 0x36 ///< MAX17048 default i2c address
+MAX17048Component = max17048_ns.class_(
+    "MAX17048Component", cg.PollingComponent, i2c.I2CDevice
+)
+
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(MAX17048Component),
+            cv.Optional(CONF_VOLTAGE): sensor.sensor_schema(
+                unit_of_measurement=UNIT_VOLT,
+                accuracy_decimals=2,
+                device_class=DEVICE_CLASS_VOLTAGE,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+        }
+    )
+    .extend(cv.polling_component_schema("60s"))
+    .extend(i2c.i2c_device_schema(0x36))
+)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await i2c.register_i2c_device(var, config)
