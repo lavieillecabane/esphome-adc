006dc2fdd428ab9a2b4ff9a175431f66a850a575
Ludovic BOUÉ <lboue@users.noreply.github.com>
2024-05-24 10:17:44 +0200

    Create vcnl4020.cpp

---
+++ esphome/components/vcnl4020/vcnl4020.cpp
@@ -0,0 +1,13 @@
+#include "vcnl4020.h"
+#include "esphome/core/application.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace vcnl4020 {
+
+static const char *const TAG = "vcnl4020";
+static const size_t VEML_REG_SIZE = 2;
+
+}
+}  // namespace vcnl4020
+}  // namespace esphome
