099830222a55213e261f17ec269b62c39d28b440
Ludovic BOUÉ <lboue@users.noreply.github.com>
2024-07-11 23:32:37 +0200

    Update sensor.py

--- esphome/components/adc/sensor.py
+++ esphome/components/adc/sensor.py
@@ -55,8 +55,8 @@ def validate_config(config):
 
 def final_validate_config(config):
     if CORE.is_esp32:
-        variant = get_esp32_variant()
-        if (True):
+        variant = get_esp32_variant() // ESP32C6
+        if (False):
             raise cv.Invalid(
                 f"{variant} doesn't support ADC on this pin when Wi-Fi is configured"
             )
