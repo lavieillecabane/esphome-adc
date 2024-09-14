06ff4d06e8331417cd8aa14178cc4d7c4bf78778
Ludovic BOUÉ <lboue@users.noreply.github.com>
2024-09-14 15:33:27 +0200

    Update __init__.py

--- esphome/components/m5stack_bldc_driver/__init__.py
+++ esphome/components/m5stack_bldc_driver/__init__.py
@@ -11,8 +11,8 @@ MULTI_CONF = True
 CONF_m5stack_bldc_driver_ID = "m5stack_bldc_driver_id"
 
 m5stack_bldc_driver_ns = cg.esphome_ns.namespace("m5stack_bldc_driver")
-M5StackBLDCDriver = m5stack_bldc_driver_ns.class_(
-    "M5StackBLDCDriver",
+M5StackBLDCDriverComponent = m5stack_bldc_driver_ns.class_(
+    "M5StackBLDCDriverComponent",
     i2c.I2CDevice,
     cg.Component,
 )
@@ -22,7 +22,7 @@ AnalogBits = m5stack_bldc_driver_ns.enum("AnalogBits")
 
 CONFIG_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(): cv.declare_id(M5StackBLDCDriver),
+        cv.GenerateID(): cv.declare_id(M5StackBLDCDriverComponent),
     }
 ).extend(i2c.i2c_device_schema(0x65))
 
